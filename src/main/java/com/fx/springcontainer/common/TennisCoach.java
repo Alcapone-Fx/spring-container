457f4c213b6e098a11b20920e414299ad0c6cdbf
Felix Navas <navasfx19@gmail.com>
2023-12-08 15:45:10 -0600

    :sparkles: Bean life cycle methods

--- src/main/java/com/fx/springcontainer/common/TennisCoach.java
+++ src/main/java/com/fx/springcontainer/common/TennisCoach.java
@@ -1,5 +1,7 @@
 package com.fx.springcontainer.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,6 +9,17 @@ public class TennisCoach implements Coach {
     public TennisCoach() {
         System.out.println("Tennis constructor: " + getClass().getSimpleName());
     }
+
+    @PostConstruct
+    public void doStartUpStuff() {
+        System.out.println("StartUp stuff: " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doCleanUpStuff() {
+        System.out.println("CleanUp stuff" + getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "This is the excercise for Tennis!";
